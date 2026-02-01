62b3f7f35653b2a088eb94ff90c129cd963e79b1
junida99 <junida99@daum.net>
2026-02-01 12:02:53 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 172 ms, Memory: 40388 KB -BaekjoonHub

--- Python/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ Python/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -2,7 +2,7 @@ import sys
 import heapq
 
 n, k = map(int, sys.stdin.readline().split())
-distance = [float('inf') for _ in range(1000001)]
+distance = [float('inf') for _ in range(100001)]
 priority_queue = [(0, n)]
 while priority_queue:
     total_weight, point = heapq.heappop(priority_queue)
@@ -12,13 +12,13 @@ while priority_queue:
             heapq.heappush(priority_queue, (total_weight, k))
             distance[k] = total_weight
     else:
-        if distance[point - 1] > total_weight + 1 and point - 1 > -1:
+        if distance[point - 1] > total_weight + 1 and point-1 > -1:
             heapq.heappush(priority_queue, (total_weight+1, point-1))
             distance[point - 1] = total_weight + 1
         if distance[point + 1] > total_weight + 1:
             heapq.heappush(priority_queue, (total_weight+1, point+1))
             distance[point + 1] = total_weight + 1
-        if distance[2*point] > total_weight:
+        if 2*point <= 100000 and distance[2*point] > total_weight:
             heapq.heappush(priority_queue, (total_weight, 2*point))
             distance[2*point] = total_weight
 print(distance[k])
