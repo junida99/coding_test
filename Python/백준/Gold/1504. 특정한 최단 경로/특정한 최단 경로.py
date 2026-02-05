cc028f25821d7087f6d40c3454dac0bcfb7f8925
junida99 <junida99@daum.net>
2026-02-05 09:38:48 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 2148 ms, Memory: 65148 KB -BaekjoonHub

---
+++ Python/백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,32 @@
+import sys, heapq
+
+n, e = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+
+for i in range(e):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+v1, v2 = map(int, sys.stdin.readline().split())
+
+def dijkstra(graph, start, end):
+    weight_list = [float("inf") for i in range(n+1)]
+    priority_queue = [(start, 0)]
+    weight_list[start] = 0
+    while priority_queue:
+        curr, curr_weight = heapq.heappop(priority_queue)
+        for node, weight in graph[curr]:
+            total_weight = curr_weight + weight
+            if total_weight < weight_list[node]:
+                weight_list[node] = total_weight
+                priority_queue.append((node, total_weight))
+    return weight_list[end]
+
+first = dijkstra(graph, 1, v1) + dijkstra(graph, v1, v2) + dijkstra(graph, v2, n)
+second = dijkstra(graph, 1, v2) + dijkstra(graph, v2, v1) + dijkstra(graph, v1, n)
+ans = min(first, second)
+if ans == float('inf'):
+    print(-1)
+else:
+    print(ans)
