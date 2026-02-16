b3b5f6e9f30284c9f402c11f45c2805cb9b229bf
junida99 <junida99@daum.net>
2026-02-16 19:18:01 +0900

    [Gold IV] Title: 플로이드, Time: 520 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Gold/11404. 플로이드/플로이드.py
+++ Python/백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,18 @@
 import sys
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
 for i in range(m):
     a, b, c = map(int, sys.stdin.readline().split())
-    if graph[a][b] == 0:
-        graph[a][b] = c
-    elif graph[a][b] > c:
-        graph[a][b] = c
+    graph[a][b] = min(graph[a][b], c)
+
+for i in range(1, n+1):
+    graph[i][i] = 0
 
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if i != j:
-                if graph[i][j] == 0:
-                    if graph[i][k] != 0 and graph[k][j] != 0:
-                        graph[i][j] = graph[i][k] + graph[k][j]
-                elif graph[i][k] != 0 and graph[k][j] != 0 and graph[i][j] > graph[i][k] + graph[k][j]:
-                    graph[i][j] = graph[i][k] + graph[k][j]
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
 for i in range(1, n+1):
-    print(" ".join(map(str,graph[i][1:])))
+    print(" ".join(map(lambda x: str(x) if x != float('inf') else '0', graph[i][1:])))
