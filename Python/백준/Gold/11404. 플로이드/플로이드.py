c7636d9b9445e409cd6c30308ecf8e0731cf95ed
junida99 <junida99@daum.net>
2026-02-16 11:59:02 +0900

    [Gold IV] Title: 플로이드, Time: 632 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Gold/11404. 플로이드/플로이드.py
+++ Python/백준/Gold/11404. 플로이드/플로이드.py
@@ -1,34 +1,23 @@
-import sys, heapq
-
+import sys
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-graph = [[] for _ in range(n+1)]
+graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
 for i in range(m):
-    # a: 출발도시, b: 도착도시, c:cost
     a, b, c = map(int, sys.stdin.readline().split())
-    graph[a].append((b, c))
+    if graph[a][b] == 0:
+        graph[a][b] = c
+    elif graph[a][b] > c:
+        graph[a][b] = c
 
-def dijkstra(start):
-    total_cost = [float("Inf") for _ in range(n+1)]
-    priority_queue = []
-    heapq.heappush(priority_queue, (0, start))
-    total_cost[start] = 0
-    while priority_queue:
-        cost, curr = heapq.heappop(priority_queue)
-        if cost > total_cost[curr]:
-            continue
-        for next_city, next_cost in graph[curr]:
-            temp_cost = cost + next_cost
-            if temp_cost < total_cost[next_city]:
-                heapq.heappush(priority_queue, (temp_cost, next_city))
-                total_cost[next_city] = temp_cost
-    return_val = []
+for k in range(1, n+1):
     for i in range(1, n+1):
-        if total_cost[i] == float("Inf"):
-            return_val.append(0)
-        else:
-            return_val.append(total_cost[i])
-    return return_val
+        for j in range(1, n+1):
+            if i != j:
+                if graph[i][j] == 0:
+                    if graph[i][k] != 0 and graph[k][j] != 0:
+                        graph[i][j] = graph[i][k] + graph[k][j]
+                elif graph[i][k] != 0 and graph[k][j] != 0 and graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
 for i in range(1, n+1):
-    print(" ".join(map(str,dijkstra(i))))
+    print(" ".join(map(str,graph[i][1:])))
