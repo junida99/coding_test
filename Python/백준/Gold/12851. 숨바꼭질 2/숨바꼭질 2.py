5781cac137d6e2776cfbbe13bbdfc596b0a28a46
junida99 <junida99@daum.net>
2026-02-17 11:02:55 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 164 ms, Memory: 45480 KB -BaekjoonHub

--- Python/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ Python/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -13,17 +13,20 @@ def bfs(start, target):
         if curr == target:
             break
         if curr+1 <= target and visited[curr+1][0] >= visited[curr][0] + 1 :
-            queue.append(curr+1)
-            visited[curr+1][0] = visited[curr][0] + 1
-            visited[curr+1][1] += 1
+            visited[curr+1][1] += visited[curr][1]
+            if visited[curr+1][0] > visited[curr][0] + 1:
+                queue.append(curr+1)
+                visited[curr+1][0] = visited[curr][0] + 1
         if curr-1 >= 0 and visited[curr-1][0] >= visited[curr][0] + 1:
-            queue.append(curr-1)
-            visited[curr-1][0] = visited[curr][0] + 1
-            visited[curr-1][1] += 1
+            visited[curr-1][1] += visited[curr][1]
+            if visited[curr-1][0] > visited[curr][0] + 1:
+                queue.append(curr-1)
+                visited[curr-1][0] = visited[curr][0] + 1
         if 2*curr < 2*target and 2*curr <= 100000 and visited[2*curr][0] >= visited[curr][0] + 1:
-            queue.append(2*curr)
-            visited[2*curr][0] = visited[curr][0] + 1
-            visited[2*curr][1] += 1
+            visited[2*curr][1] += visited[curr][1]
+            if visited[2*curr][0] > visited[curr][0] + 1:
+                queue.append(2*curr)
+                visited[2*curr][0] = visited[curr][0] + 1
 
 bfs(n, k)
 print(visited[k][0])
